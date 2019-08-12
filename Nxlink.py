47bf8fe37cbcf2ec001cf81e2d9c8c0c0c5c19f6
KrankRival <krankshuffle@gmail.com>
2019-08-12 18:53:18 +0300

    Update 1

--- Nxlink.py
+++ Nxlink.py
@@ -17,39 +17,68 @@ class MyWindow(Gtk.Window):
 
     def init_ui(self):    
 
-        grid = Gtk.Grid()
-        grid.set_column_spacing(5)
-        self.add(grid)        
+        box = Gtk.Box(spacing=6)
+        self.add(box)
 
-        entry = Gtk.Entry()
-        entry.connect("key-release-event", self.on_key_release)
-        entry.set_text("192.168.")
-
-        button1 = Gtk.Button("Send NRO")
-        button1.connect("clicked", self.whenbutton1_clicked)
-
-        grid.attach(entry, 0, 0, 1, 1)
-        grid.attach(button1, 2, 0, 1, 1)
-
-        self.label = Gtk.Label("")
-        self.label.set_width_chars(15)
-
-        #grid.attach(self.label, 0, 1, 1, 1)
-
-        self.set_border_width(5)
 
         self.set_title("NxLink PUI by KranK/KuranKu")
-        self.set_default_size(350, 100)
+        self.set_default_size(300, 30)
         self.connect("destroy", Gtk.main_quit)
 
+        OpenNroButtonn = Gtk.Button("Choose File")
+        OpenNroButtonn.connect("clicked", self.on_file_clicked)
+        box.add(OpenNroButtonn)
+
+        IpAdressBox = Gtk.Entry()
+        IpAdressBox.connect("key-release-event", self.on_key_release)
+        IpAdressBox.set_text("192.168.")
+        box.add(IpAdressBox)
+
+        SendNroButton = Gtk.Button("Send NRO")
+        SendNroButton.connect("clicked", self.SendNroButton_clicked)
+        box.add(SendNroButton)
+
+        self.NROlabel = Gtk.Label("")
+        self.NROlabel.set_width_chars(12)
+
+        self.IpAdress = Gtk.Label("")
+        self.IpAdress.set_width_chars(12)
+
+
+
+    def on_file_clicked(self, widget):
+        dialog = Gtk.FileChooserDialog("Please choose a file", self,
+            Gtk.FileChooserAction.OPEN,
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+             Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+             
         
+        self.add_filters(dialog)
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            print("Open clicked")
+            print("File selected: " + dialog.get_filename())
+            self.NROlabel.set_text(dialog.get_filename())
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+
+        dialog.destroy()
+
+    def add_filters(self, dialog):
+
+        filter_any = Gtk.FileFilter()
+        filter_any.set_name("Switch ? (.NRO)")
+        filter_any.add_pattern("*.[Nn][Rr][Oo]")
+        dialog.add_filter(filter_any)
 
     def on_key_release(self, widget, event):
-        self.label.set_text(widget.get_text())
+        self.IpAdress.set_text(widget.get_text())
 
-    def whenbutton1_clicked(self, button):
-        print (self.label.get_label())
-        os.system("nxlink -s -a " + self.label.get_label() + " Atmosphere_ToolBox.nro")
+    def SendNroButton_clicked(self, button):
+        print ("Ip Adress is: " + self.IpAdress.get_label())
+        print ("Sent Command: " + "nxlink -s -a " + self.IpAdress.get_label() + self.NROlabel.get_label())
+        os.system("nxlink -s -a " + self.IpAdress.get_label() + " " + self.NROlabel.get_label())
 
 
 win = MyWindow()
